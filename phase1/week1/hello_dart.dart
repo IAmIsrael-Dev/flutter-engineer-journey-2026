93e5bd0b20d98f4a02b8bfbd29e243a74c67d3fc
Adrian <adrianhooper095@gmail.com>
2026-08-02 21:52:38 +0100

    progress

---
+++ phase1/week1/hello_dart.dart
@@ -0,0 +1,10 @@
+var name = "israel";
+var age = 34;
+var height = 3.89;
+String jobRole = 'Banker';
+int misCode = 00300721;
+
+void main() {
+  print("my name is $name, I am $age years old, my height is $height meters");
+  print("I work as a $jobRole and my employee code is $misCode");
+}
