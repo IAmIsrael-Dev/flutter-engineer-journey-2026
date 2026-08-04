fe6d29e2844a200a2bad14e217f3d5e6dedae125
Adrian <adrianhooper095@gmail.com>
2026-08-04 20:49:18 +0100

    day 1 done

--- phase1/week1/hello_dart.dart
+++ phase1/week1/hello_dart.dart
@@ -4,7 +4,70 @@ var height = 3.89;
 String jobRole = 'Banker';
 int misCode = 00300721;
 
+final String animal = 'cat';
+
+String? surName;
+
+double add(double a, double b) {
+  return a + b;
+}
+
+double subtract(double a, double b) {
+  return a - b;
+}
+
+double multiply(double a, double b) {
+  return a * b;
+}
+
+double divide(double a, double b) {
+  return a / b;
+}
+
+dynamic calculator(double a, double b, String operation) {
+  switch (operation) {
+    case 'add':
+      return add(a, b);
+    case 'subtract':
+      return subtract(a, b);
+    case 'multiply':
+      return multiply(a, b);
+    case 'divide':
+      return divide(a, b);
+    default:
+      return "invslid operation";
+  }
+}
+
+dynamic calculator2(double a, double b, String operation) {
+  if (operation == 'add') {
+    return add(a, b);
+  } else if (operation == 'subtract') {
+    return subtract(a, b);
+  } else if (operation == 'multiply') {
+    return multiply(a, b);
+  } else if (operation == 'divide') {
+    return divide(a, b);
+  } else {
+    return "invalid operation";
+  }
+}
+
+const Object i = 3;
+const list = [i as int];
+const map = {if (i is int) i: 'int'};
+
+bool isEven(int num) {
+  return num % 2 == 0;
+}
+
+List<int> numVar = [1,2,3,4,5,6];
+
+
+
+
 void main() {
-  print("my name is $name, I am $age years old, my height is $height meters");
-  print("I work as a $jobRole and my employee code is $misCode");
+  var [a,b,c,d,e,f] = numVar;
+  print('$a, $b, $c, $d, $e, $f');
+  
 }
